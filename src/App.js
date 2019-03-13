715afb2bdfce7814afd1a195dcaf73e0486dfa2a
Christina Ma <christinayyma@tamu.edu>
2019-03-13 00:31:27 -0500

    finished website, ask juan to update if necessary. thank u, next

--- src/App.js
+++ src/App.js
@@ -1,57 +1,132 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 import { Button, Grid, TextField, Typography, FormControl, Radio, 
-         FormControlLabel, RadioGroup, FormLabel, Switch } from '@material-ui/core';
-import axios from 'axios';
+         FormControlLabel, RadioGroup, FormLabel, Switch,
+         ListItemText, Paper, GridList, GridListTile } from '@material-ui/core';
 
 class App extends Component {
     constructor(props) {
         super(props);
-            this.state = {
+            this.state = { 
                 city: '',
                 zip: '',
-                rating: '5',
+                rating: '*',
                 alcohol: false,
+                categories: '',
                 errorMessage: '',
+                data: [],
+                hasSearched: false,
+                resultsLoading: false,
         }
     }
 
     handleCityChange = (event) => {
-        this.setState({
-            city: event.target.value
-        });
+        this.setState({ city: event.target.value });
     }
 
     handleZipChange = (event) => {
-        this.setState({
-            zip: event.target.value
-        });
+        this.setState({ zip: event.target.value });
     }
 
     handleRatingChange = (event) => {
-       this.setState({
-           rating: event.target.value
-       })
+       this.setState({ rating: event.target.value });
     }
 
     handleAlcoholChange = (event) => {
-        this.setState({
-            alcohol: !this.state.alcohol
-        })
+        this.setState({ alcohol: !this.state.alcohol });
+    }
+
+    handleCategoriesChange = (event) => {
+        this.setState({ categories: event.target.value });
     }
 
     handleSubmit = () => {
-        const { city } = this.state;
+        const { city, zip, rating, alcohol, categories } = this.state;
+        this.setState({ hasSearched: true });
+
         if (city !== '') {
-            this.setState({
-                errorMessage: ''
-            });
-            console.log(this.state);
+            this.setState({ errorMessage: '' });
+
+            var params = new URLSearchParams();
+            params.append('q', 'address:' + city);
+            params.append('rows', '1000');
+
+            if (zip !== '') {
+                params.append('fq', 'zipcode:' + zip);
+            }
+
+            if (rating !== '*'){
+                params.append('fq', 'rating:' + rating);
+            }
+
+            if (alcohol) {
+                params.append('fq', 'categories:( Beer OR Wine OR Bar OR Bars)');
+            }
+
+            if (categories) {
+                params.append('fq', 'categories: ' + categories);
+            }
+
+            this.setState({ resultsLoading: true });
+            axios.get('http://localhost:8983/solr/texas_restaurants/select', { params })
+                .then((response) => {
+                  this.setState({ data: response.data.response.docs })
+                })
+                .catch(function (error) {
+                  console.error(error);
+                });
+            this.setState({ resultsLoading: false });
+
         } else {
-            this.setState({
-                errorMessage: 'Please type in a city!'
-            });
+            this.setState({ data: [], errorMessage: 'Please type in a city!' });
+        }
+    }
+
+    renderData = () => {
+        const { data, hasSearched, resultsLoading } = this.state;
+
+        if (data.length !== 0) {
+            return (
+                <Grid container justify={'center'}>
+                    <Grid item xs={8}>
+                        <GridList cols={3} spacing={50}>
+                            {data.map((value) => (
+                                <GridListTile key={value.id}>
+                                    <Paper>
+                                        <ListItemText 
+                                            primary={value.name}
+                                            secondary={
+                                                <React.Fragment>
+                                                    Address: {value.address}
+                                                    <br/>
+                                                    Categories: {value.categories.join(', ')}
+                                                    <br/>
+                                                    Rating: {value.rating}
+                                                </React.Fragment>
+                                            }
+                                        />
+                                    </Paper>
+                                </GridListTile>
+                            ))}
+                        </GridList>
+                    </Grid>
+                </Grid>
+            );
+        } else if (hasSearched && !resultsLoading) {
+            return (
+                <div>
+                    <br/>
+                    <Typography 
+                        component="h6" 
+                        variant="h6" 
+                        gutterBottom
+                    >
+                        {'No Results'}
+                    </Typography>
+                </div>
+            );
         }
     }
 
@@ -73,7 +148,7 @@ class App extends Component {
                             fullWidth
                             color="secondary"
                             label="City"
-                            value={this.state.searchValue}
+                            value={this.state.city}
                             onChange={(event) => this.handleCityChange(event)}
                             margin="normal"
                             variant="outlined"
@@ -82,10 +157,25 @@ class App extends Component {
                     <Grid item xs={2}>
                         <TextField
                             fullWidth
-                            color={''}
+                            type={'number'}
                             label="Zip Code"
-                            value={this.state.searchValue}
+                            value={this.state.zip}
                             onChange={(event) => this.handleZipChange(event)}
+                            margin="normal"
+                            variant="outlined"
+                        />
+                    </Grid>
+                </Grid>
+
+                <br/>
+
+                <Grid container justify={'center'} spacing={16}>
+                    <Grid item xs={2}>
+                        <TextField
+                            fullWidth
+                            label="Categories"
+                            value={this.state.categories}
+                            onChange={(event) => this.handleCategoriesChange(event)}
                             margin="normal"
                             variant="outlined"
                         />
@@ -108,6 +198,7 @@ class App extends Component {
                         <FormControlLabel value="3" labelPlacement={'bottom'} control={<Radio />} label="3 Stars" />
                         <FormControlLabel value="2" labelPlacement={'bottom'} control={<Radio />} label="2 Stars" />
                         <FormControlLabel value="1" labelPlacement={'bottom'} control={<Radio />} label="1 Stars" />
+                        <FormControlLabel value="*" labelPlacement={'bottom'} control={<Radio />} label="Any" />
                     </RadioGroup>
                 </FormControl>
 
@@ -137,6 +228,8 @@ class App extends Component {
                     </Grid>
                 </Grid>
 
+                <br />
+
                 {this.state.errorMessage ? 
                     <Typography 
                         component="h6" 
@@ -145,6 +238,24 @@ class App extends Component {
                     >
                         {this.state.errorMessage}
                     </Typography> : null}
+
+                <br/>
+
+                { this.state.data.length !== 0 ? (
+                    <Typography 
+                        component="h6" 
+                        variant="h6" 
+                        gutterBottom
+                    >
+                        {'Number of Results: ' + this.state.data.length}
+                    </Typography>
+                    
+                ) : null } 
+
+                <br/>
+
+                { this.renderData() }
+
             </div>
         );
     }
