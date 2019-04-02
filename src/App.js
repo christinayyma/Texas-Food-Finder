546b8285e78fb0059d5a8fcc4f287bcb18798e70
Amy Hughes <Amy@Amys-MacBook-Pro-2.local>
2019-04-02 10:23:58 -0500

    Page switching

--- src/App.js
+++ src/App.js
@@ -1,12 +1,92 @@
-import React, { Component } from 'react';
+import React, {Component} from "react";
+
+
 import axios from 'axios';
 import logo from './cartoon-man.png';
+
 import './App.css';
 import { Button, Grid, TextField, Typography, FormControl, Radio, 
          FormControlLabel, RadioGroup, FormLabel, Switch,
-         ListItemText, Paper, GridList, GridListTile } from '@material-ui/core';
+         ListItemText, Paper, GridList, GridListTile} from '@material-ui/core';
+import {BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/topics">Topics</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} />
+    </div>
+  </Router>
+);
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic} />
+    <Route
+      exact
+      path={match.url}
+      render={() => <h3>Please select a topic.</h3>}
+    />
+  </div>
+);
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+);
+
+export default BasicExample;
+
+
+
+
+
+
+
 
 class App extends Component {
+    
     constructor(props) {
         super(props);
             this.state = { 
@@ -134,6 +214,8 @@ class App extends Component {
         return ( 
             <div className="App App-header">
                 <img src={logo} className="App-logo" alt="logo" />
+                <br>
+                </br>
                 <Typography 
                     component="h2" 
                     variant="h2" 
@@ -142,123 +224,139 @@ class App extends Component {
                     {'Texas Food Finder'}
                 </Typography>
 
-                <Grid container justify={'center'} spacing={16}>
-                    <Grid item xs={4}>
-                        <TextField
-                            fullWidth
-                            color="secondary"
-                            label="City"
-                            value={this.state.city}
-                            onChange={(event) => this.handleCityChange(event)}
-                            margin="normal"
-                            variant="outlined"
-                        />
-                    </Grid>
-                    <Grid item xs={2}>
-                        <TextField
-                            fullWidth
-                            type={'number'}
-                            label="Zip Code"
-                            value={this.state.zip}
-                            onChange={(event) => this.handleZipChange(event)}
-                            margin="normal"
-                            variant="outlined"
-                        />
-                    </Grid>
-                </Grid>
+                <br>
+                </br>
+               
+                <Button href="#">Quiz</Button>
+                <Button type="submit">Random</Button>
 
-                <br/>
+              
+                
+            
 
-                <Grid container justify={'center'} spacing={16}>
-                    <Grid item xs={2}>
-                        <TextField
-                            fullWidth
-                            label="Categories"
-                            value={this.state.categories}
-                            onChange={(event) => this.handleCategoriesChange(event)}
-                            margin="normal"
-                            variant="outlined"
-                        />
-                    </Grid>
-                </Grid>
+                </div>
 
-                <br/>
 
-                <FormControl component="fieldset">
-                    <FormLabel component="legend">Rating</FormLabel>
-                    <RadioGroup
-                        row
-                        aria-label="Gender"
-                        name="gender1"
-                        value={this.state.rating}
-                        onChange={this.handleRatingChange}
-                    >
-                        <FormControlLabel value="5" labelPlacement={'bottom'} control={<Radio />} label="5 Stars" />
-                        <FormControlLabel value="4" labelPlacement={'bottom'} control={<Radio />} label="4 Stars" />
-                        <FormControlLabel value="3" labelPlacement={'bottom'} control={<Radio />} label="3 Stars" />
-                        <FormControlLabel value="2" labelPlacement={'bottom'} control={<Radio />} label="2 Stars" />
-                        <FormControlLabel value="1" labelPlacement={'bottom'} control={<Radio />} label="1 Stars" />
-                        <FormControlLabel value="*" labelPlacement={'bottom'} control={<Radio />} label="Any" />
-                    </RadioGroup>
-                </FormControl>
 
-                <br />
+    //         //     <Grid container justify={'center'} spacing={16}>
+    //         //         <Grid item xs={4}>
+    //         //             <TextField
+    //         //                 fullWidth
+    //         //                 color="secondary"
+    //         //                 label="City"
+    //         //                 value={this.state.city}
+    //         //                 onChange={(event) => this.handleCityChange(event)}
+    //         //                 margin="normal"
+    //         //                 variant="outlined"
+    //         //             />
+    //         //         </Grid>
+    //         //         <Grid item xs={2}>
+    //         //             <TextField
+    //         //                 fullWidth
+    //         //                 type={'number'}
+    //         //                 label="Zip Code"
+            //                 value={this.state.zip}
+            //                 onChange={(event) => this.handleZipChange(event)}
+            //                 margin="normal"
+            //                 variant="outlined"
+            //             />
+            //         </Grid>
+            //     </Grid>
 
-                <FormControl component="fieldset">
-                    <FormLabel component="legend">Alcohol</FormLabel>
-                    <Switch
-                            checked={this.state.alcohol}
-                            onChange={this.handleAlcoholChange}
-                            value={this.state.alcohol}
-                        />
-                </FormControl>
+            //     <br/>
 
-                <br/>
+            //     <Grid container justify={'center'} spacing={16}>
+            //         <Grid item xs={2}>
+            //             <TextField
+            //                 fullWidth
+            //                 label="Categories"
+            //                 value={this.state.categories}
+            //                 onChange={(event) => this.handleCategoriesChange(event)}
+            //                 margin="normal"
+            //                 variant="outlined"
+            //             />
+            //         </Grid>
+            //     </Grid>
 
-                <Grid container justify={'center'}>
-                    <Grid item xs={1}>
-                        <Button
-                            fullWidth
-                            variant={'contained'}
-                            color={'secondary'}
-                            onClick={this.handleSubmit}
-                        >
-                            {'Search'}
-                        </Button>
-                    </Grid>
-                </Grid>
+            //     <br/>
 
-                <br />
+            //     <FormControl component="fieldset">
+            //         <FormLabel component="legend">Rating</FormLabel>
+            //         <RadioGroup
+            //             row
+            //             aria-label="Gender"
+            //             name="gender1"
+            //             value={this.state.rating}
+            //             onChange={this.handleRatingChange}
+            //         >
+            //             <FormControlLabel value="5" labelPlacement={'bottom'} control={<Radio />} label="5 Stars" />
+            //             <FormControlLabel value="4" labelPlacement={'bottom'} control={<Radio />} label="4 Stars" />
+            //             <FormControlLabel value="3" labelPlacement={'bottom'} control={<Radio />} label="3 Stars" />
+            //             <FormControlLabel value="2" labelPlacement={'bottom'} control={<Radio />} label="2 Stars" />
+            //             <FormControlLabel value="1" labelPlacement={'bottom'} control={<Radio />} label="1 Stars" />
+            //             <FormControlLabel value="*" labelPlacement={'bottom'} control={<Radio />} label="Any" />
+            //         </RadioGroup>
+            //     </FormControl>
 
-                {this.state.errorMessage ? 
-                    <Typography 
-                        component="h6" 
-                        variant="h6" 
-                        gutterBottom
-                    >
-                        {this.state.errorMessage}
-                    </Typography> : null}
+            //     <br />
 
-                <br/>
+            //     <FormControl component="fieldset">
+            //         <FormLabel component="legend">Alcohol</FormLabel>
+            //         <Switch
+            //                 checked={this.state.alcohol}
+            //                 onChange={this.handleAlcoholChange}
+            //                 value={this.state.alcohol}
+            //             />
+            //     </FormControl>
 
-                { this.state.data.length !== 0 ? (
-                    <Typography 
-                        component="h6" 
-                        variant="h6" 
-                        gutterBottom
-                    >
-                        {'Number of Results: ' + this.state.data.length}
-                    </Typography>
+            //     <br/>
+
+            //     <Grid container justify={'center'}>
+            //         <Grid item xs={1}>
+            //             <Button
+            //                 fullWidth
+            //                 variant={'contained'}
+            //                 color={'secondary'}
+            //                 onClick={this.handleSubmit}
+            //             >
+            //                 {'Search'}
+            //             </Button>
+            //         </Grid>
+            //     </Grid>
+
+            //     <br />
+
+            //     {this.state.errorMessage ? 
+            //         <Typography 
+            //             component="h6" 
+            //             variant="h6" 
+            //             gutterBottom
+            //         >
+            //             {this.state.errorMessage}
+            //         </Typography> : null}
+
+            //     <br/>
+
+            //     { this.state.data.length !== 0 ? (
+            //         <Typography 
+            //             component="h6" 
+            //             variant="h6" 
+            //             gutterBottom
+            //         >
+            //             {'Number of Results: ' + this.state.data.length}
+            //         </Typography>
                     
-                ) : null } 
+            //     ) : null } 
 
-                <br/>
+            //     <br/>
 
-                { this.renderData() }
+            //     { this.renderData() }
 
-            </div>
+            // </div>
+           
+            
         );
-    }
+   }
 }
 
-export default App;
+//export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Router, Route, IndexRoute,browserHistory} from "react-router-dom";
+
+// import {Root} from "./components/Root";
+// import {Home} from "./components/Home";
+// import {Location} from "./components/Location";
+
 
 ReactDOM.render(<App/>, document.getElementById('root'));
+
+// class App extends React.Component {
+//     render() {
+//         return(
+//             <Router history = {browserHistory}>
+//             <Route path ={"/"} component = {Root}>
+//                 <IndexRoute component={Home} />
+//                 <Route path = {"Home"} component = {Home} />
+//                 <Route path = {"Location"} component = {Location} />
+//             </Route>
+//             <Route path = {"home-single"} component= {Home}/>
+//             </Router>
+
+//         );
+//     }
+// }
+
+// render(<App />, window.document.getElementById('app'));
 
 
